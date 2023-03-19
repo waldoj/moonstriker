f097f472e8b70472ac7b73de4545ca97bae98469
Waldo Jaquith <waldo@jaquith.org>
2023-03-19 09:13:35 -0400

    Adapt further to accommodate new clips
    Toward #1.

--- processor.sh
+++ processor.sh
@@ -4,23 +4,17 @@ VIDEO_FILE="video.m4v"
 CAPTIONS_FILE="captions.srt"
 CLIPS_FILE="clips.csv"
 
-timestamp() {
-    gdate -d "$1" "+%s.%3N"
-}
-
 # Iterate through every row in the CSV file, getting the start and end timestamps for each
 csvcut -c 2,4 "$CLIPS_FILE" |while read -r row
 do
     
-    IFS=$'\t' read -ra columns <<< "$row"
+    IFS=$',' read -ra columns <<< "$row"
     
-    START=$(timestamp "${columns[0]}")
-    END=$(timestamp "${columns[1]}")
-    DURATION=$(echo "$END - $START" |bc)
+    DURATION=$(echo "${columns[1]} - ${columns[0]}" |bc)
 
     # Create the video clip
-    ffmpeg -nostdin -loglevel error -ss "${SRT[0]}" -t "$DURATION" -copyts -i "$VIDEO_FILE" \
-        -i "$CAPTIONS_FILE" -ss "${SRT[0]}" -t "$DURATION" -vf \
+    ffmpeg -nostdin -loglevel error -ss "${columns[0]}" -t "$DURATION" -copyts -i "$VIDEO_FILE" \
+        -i "$CAPTIONS_FILE" -ss "${columns[0]}" -t "$DURATION" -vf \
         subtitles="$CAPTIONS_FILE" -scodec mov_text clips/"$i".m4v
     i=$((1 + i))
 
