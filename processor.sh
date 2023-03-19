6a06e2d73cd574791cc7d5a2358ab966e3da8c88
Waldo Jaquith <waldo@jaquith.org>
2023-03-18 21:16:03 -0400

    Adapt processor for new file
    Toward #1.

--- processor.sh
+++ processor.sh
@@ -2,34 +2,26 @@
 
 VIDEO_FILE="video.m4v"
 CAPTIONS_FILE="captions.srt"
+CLIPS_FILE="clips.csv"
 
 timestamp() {
     gdate -d "$1" "+%s.%3N"
 }
 
-# Get a list of all time ranges, save it as an array
-SRTS=()
-while IFS= read -r line; do
-    SRTS+=( "$line" )
-done < <( grep " --> " {$CAPTIONS_FILE} |cut -d " " -f 1,3 )
-
-# Iterate through every caption time range and extract that clip
-i=1
-for SRT in "${SRTS[@]}"
+# Iterate through every row in the CSV file, getting the start and end timestamps for each
+csvcut -c 2,4 "$CLIPS_FILE" |while read -r row
 do
-    # Replace commas with periods, for decimal seconds
-    SRT=${SRT//,/.}
     
-    # Break up the range into a start and a duration
-    IFS=' ' read -r -a SRT <<< "$SRT"
-
-    START=$(timestamp "${SRT[0]}")
-    END=$(timestamp "${SRT[1]}")
+    IFS=$'\t' read -ra columns <<< "$row"
+    
+    START=$(timestamp "${columns[0]}")
+    END=$(timestamp "${columns[1]}")
     DURATION=$(echo "$END - $START" |bc)
 
     # Create the video clip
-    ffmpeg -loglevel error -ss "${SRT[0]}" -t "$DURATION" -copyts -i "$VIDEO_FILE" \
+    ffmpeg -nostdin -loglevel error -ss "${SRT[0]}" -t "$DURATION" -copyts -i "$VIDEO_FILE" \
         -i "$CAPTIONS_FILE" -ss "${SRT[0]}" -t "$DURATION" -vf \
         subtitles="$CAPTIONS_FILE" -scodec mov_text clips/"$i".m4v
     i=$((1 + i))
+
 done
